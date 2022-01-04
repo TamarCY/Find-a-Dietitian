15b4809b37bcbf0950f715004df182e40e7dc5df
tamarcy <cohen.tamar@gmail.com>
2022-01-04 21:23:41 +0200

    no results page styled

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -3,39 +3,48 @@ import "./Results.css";
 import logo from "../../Assets/Images/logo.png"
 import DietitianCard from "../../Components/DietitianCard/DietitianCard";
 import { Box, Container } from "@mui/material";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 // TODO: deal with errors when page refreshed
 
-const  Results = ({searchResults}) =>{
+const Results = ({ searchResults }) => {
     if (searchResults.length === 0) {
-        return <div>
+        return <div className="Results-none">
+            <Link to="/" >
+                <div className="Results-logo">
+                    <img src={logo} alt="logo" />
+                </div>
+            </Link>
             <div>
-            אין תוצאות מתאימות לחיפוש
-        </div>
+                אין תוצאות מתאימות לחיפוש
+            </div>
+            <div className="Results-new">
             <Link to="/search">
                 חיפוש חדש
             </Link>
             </div>
+        </div>
     }
     const renderCards = () => {
-        return searchResults.map((item)=> {return (<div key={item.id} className="Results-card" ><DietitianCard data={item} isEditMode={false}/></div>)})
+        return searchResults.map((item) => { return (<div key={item.id} className="Results-card" ><DietitianCard data={item} isEditMode={false} /></div>) })
     }
 
-    if(!searchResults) return <div></div>;
+    if (!searchResults) return <div></div>;
     return (
         <>
-        <Link to="/" >
-                <div className="SearchToEdit-logo">
+            <Link to="/" >
+                <div className="Results-logo">
                     <img src={logo} alt="logo" />
                 </div>
             </Link>
-        <div className="Results-container">
-          {renderCards()}
-        </div>
+            <div className="Results-container">
+                {renderCards()}
+            </div>
         </>
     )
 }
+
+// TODO: fix the code DRY
 
 
 export default Results;
